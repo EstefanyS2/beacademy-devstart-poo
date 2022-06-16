99cc46ed49e96ee41e3e5478effba7e053d2b490
Estefany Alessandra <k.estefanyalessandra@gmail.com>
2022-06-16 01:05:00 -0300

    master

--- modulo02/aula02/index.php
+++ modulo02/aula02/index.php
@@ -5,12 +5,14 @@ ini_set('display_errors', 1);
 include 'Produto.php';
 
 $p1 = new Produto();
-$p1->nome = 'Tenis para corrida';
-$p1->valor = 299;
+$p1->setNome('tenis para corrida');
+$p1->setValor(299);
 
 $p2 = new Produto();
-$p2->nome = 'Calça Jeans';
-$p2->valor = -100;
+$p2->setNome('Saia Jeans');
+$p2->setValor('100');
+
+
 
 var_dump($p1);
 var_dump($p2);
---
+++ modulo02/aula03/Produto.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+class Produto {
+
+    //atributos
+    private string $nome;
+    private float $valor;
+    private string $descricao;
+    private readonly Categoria $categoria;
+
+    //metodo magico de construcao do objeto //invocado
+    public function __construct(string $novoNome, float $novoValor, Categoria $categoria)
+    {
+        $this->nome = $novoNome;
+        $this->valor = $novoValor;
+        $this->categoria = $categoria;
+    }
+
+    //metodos
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function setNome(string $novoNome): void 
+    {
+        $this->nome = $novoNome;
+    }
+    public function getValor(): float
+    {
+        return $this->valor;
+    }
+
+    public function setValor(float $novoValor): void
+    {
+        if ($novoValor <0) {
+            die('Ops, valor nao pode ser negativo');
+        }
+
+        $this->valor = $novoValor;
+    }
+
+    public function getDescricao(): string 
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao(string $novaDescricao): void
+    {
+        $this->descricao = $novaDescricao;
+    }
+
+    public function setCategoria(Categoria $categoria): void
+    {
+        $this->categoria = $categoria;
+    }
+}
