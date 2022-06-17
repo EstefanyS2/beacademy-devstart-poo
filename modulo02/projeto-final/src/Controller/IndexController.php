852cef1b7eb56ee68e989dac37991eba95f12a93
Estefany Alessandra <k.estefanyalessandra@gmail.com>
2022-06-16 22:30:24 -0300

    master

---
+++ modulo02/projeto-final/src/Controller/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+class IndexController {
+    public function indexAction(): void
+    {
+        echo 'Ola Mundo, estou de uma action, dentro do controller';
+    }
+}
