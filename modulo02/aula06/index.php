b225bef8790c1f87ef9f13912a1fe64e95dedfd0
Estefany Alessandra <k.estefanyalessandra@gmail.com>
2022-06-16 21:28:19 -0300

    master

--- modulo02/aula06/index.php
+++ modulo02/aula06/index.php
@@ -2,9 +2,17 @@
 
 ini_set('display_errors', 1);
 
-include 'classes/Usuario.php';
-include 'classes/Cliente.php';
-include 'classes/Gestor.php';
-include 'classes/Categoria.php';
-include 'classes/Produto.php';
-include 'classes/Config/Usuario.php';
+include 'vendor/autoload.php';
+
+use Classes\Config\Usuario;
+use Classes\Categoria;
+
+//$us1 = new Classes\usuario();
+// $us2 = new Classes\Config|Usuario();
+$c1 = new Categoria();
+
+$us2 = new Usuario();
+
+
+// var_dump($us1);
+var_dump($us2);
